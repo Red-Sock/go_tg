db9f852dc02465944bc7a51c57cd514b76dc48fd
AlexSkilled <olegthander@yandex.ru>
2023-05-29 21:05:43 +0300

    [RSI-111]: message formating

---
+++ model/response/text.go
@@ -0,0 +1,19 @@
+package response
+
+const (
+	MentionTextFormat       = "	mention"
+	HashtagTextFormat       = "hashtag"
+	CashtagTextFormat       = "cashtag"
+	BotCommandTextFormat    = "bot_command"
+	UrlTextFormat           = "url"
+	EmailTextFormat         = "email"
+	PhoneNumberTextFormat   = "phone_number"
+	BoldTextFormat          = "bold"
+	ItalicTextFormat        = "italic"
+	UnderlineTextFormat     = "underline"
+	StrikethroughTextFormat = "strikethrough"
+	CodeTextFormat          = "code"
+	PreTextFormat           = "pre"
+	TextLinkTextFormat      = "text_link"
+	TextMentionTextFormat   = "text_mention"
+)
